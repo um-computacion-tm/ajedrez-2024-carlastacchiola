d15dc67984ac329155463ba9cdb4146da1e6a1cd
Carla <cm.stacchiola@alumno.um.edu.ar>
2024-08-24 19:24:18 -0300

    test

--- main/cli.py
+++ main/cli.py
@@ -1,28 +1,28 @@
 from chess import Chess
 
-def main ():
+def main():
     chess = Chess()
-    while True:
-       play(Chess)
-    
-def play(chess):
-    try: 
-       #print(chess.show_board())
-       print("turn: ", chess.turn)
-       from_row = int(input("From row: "))
-       from_col = int(input("From col: "))
-       to_row = int(input("To Row: "))
-       to_col = int(input("To Col: "))
+    while chess.is_playing():
+        play(chess)
 
-       chess.move( 
-           from_row, 
-           from_col, 
-           to_row, 
-           to_col,
-           ) 
-       
+def play(chess):
+    try:
+        print(chess.show_board())
+        print("turn: ", chess.turn)
+        from_row = int(input("From row: "))
+        from_col = int(input("From col: "))
+        to_row = int(input("To Row: "))
+        to_col = int(input("To Col: "))
+        # :)
+        chess.move(
+            from_row,
+            from_col,
+            to_row,
+            to_col,
+        )
     except Exception as e:
-        print ("error", e)
+        print("error", e)
+
 
 if __name__ == '__main__':
     main()
