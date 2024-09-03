31bfc9fabfc93a0b3674bb89d8dc0d8929ae2bb3
Carla <cm.stacchiola@alumno.um.edu.ar>
2024-09-02 23:20:28 -0300

    rook

--- main/rook.py
+++ main/rook.py
@@ -6,3 +6,16 @@ class Rook(Piece):
             return "♜"
         if self.__color__ == "BLACK":
             return "♖"
+        
+    def get_possible_moves(self, x, y):
+        possible_moves = []
+
+        for col in range(8):
+            if not col == y:
+                possible_moves.append((x, col))
+        
+        for row in range(8):
+            if not row == x:
+                possible_moves.append((row, y))
+
+        return possible_moves
