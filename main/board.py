e1261147f6758d87b1751a09f318dd26fc3e9283
Carla <cm.stacchiola@alumno.um.edu.ar>
2024-09-03 22:03:06 -0300

    iconos rook

--- main/board.py
+++ main/board.py
@@ -8,11 +8,11 @@ class Board:
             for _ in range(8):
                 col.append(None)
             self.__positions__.append(col)
-        self.__positions__[0][0] = Rook("BLACK") # Black
-        self.__positions__[0][7] = Rook("BLACK") # Black
-        self.__positions__[7][7] = Rook("WHITE") # White
-        self.__positions__[7][0] = Rook("WHITE") # White
-
+        self.__positions__[0][0] = Rook("♜") 
+        self.__positions__[0][7] = Rook("♜") 
+        self.__positions__[7][7] = Rook("♖") 
+        self.__positions__[7][0] = Rook("♖") 
+       
     def __str__(self):
         board_str = ""
         for row in self.__positions__:
@@ -26,5 +26,6 @@ class Board:
 
     def get_piece(self, row, col):
         return self.__positions__[row][col]
-    
-    
+  
+
+  
